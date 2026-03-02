dd03d8f24d635766b13fc3c5c1a025576e179076
Musicminion <zhangziqian@sjtu.edu.cn>
2026-03-02 16:07:34 +0000

    fix: refactor GitHub sync module imports to use dynamic loading

--- services/web/modules/github-sync/index.mjs
+++ services/web/modules/github-sync/index.mjs
@@ -1,13 +1,16 @@
-import GitHubSyncRouter from './app/src/GitHubSyncRouter.mjs'
 import Modules from '../../app/src/infrastructure/Modules.mjs'
 import logger from '@overleaf/logger'
-import GitHubSyncHandler from './app/src/GitHubSyncHandler.mjs'
 /** @import { WebModule } from "../../types/web-module" */
 
 
 let GitHubSyncModule = {}
 if (process.env.GITHUB_SYNC_ENABLED === 'true') {
   logger.debug({}, 'Enabling GitHub Sync module')
+  const [{ default: GitHubSyncRouter }, { default: GitHubSyncHandler }] =
+    await Promise.all([
+      import('./app/src/GitHubSyncRouter.mjs'),
+      import('./app/src/GitHubSyncHandler.mjs'),
+    ])
 
   // Notes, this is only triggered in hard delete, no soft delete.
   // Hard delete have some time delay, usually 30 or more days after soft delete.
